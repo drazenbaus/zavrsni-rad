b459362499656d360055f0636cdfd40c9ee78fe6
Drazen Baus <drazen.baus@yahoo.com>
2019-12-26 01:59:45 +0100

    database connection

---
+++ connection/DbConnection.php
@@ -0,0 +1,30 @@
+<?php
+
+class DbConnection {
+    private $connection;
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+
+    public function __construct($servername, $dbname, $username, $password) {
+        $this->servername = $servername;
+        $this->dbname = $dbname;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function connect() {
+        try {
+            $connection = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+            
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+
+        
+        return $connection;
+    }
+
+}
