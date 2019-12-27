331fd2629ce9f0aa3f2242caf4efcd67f8d94c12
Drazen Baus <drazen.baus@yahoo.com>
2019-12-27 15:51:13 +0100

    version 2.0

--- posts.php
+++ posts.php
@@ -1,25 +1,45 @@
 <?php
     include('connection/DbConnection.php');
     include('repository/PostRepository.php');
+    include('constants/Environment.php');
 
-    $dBConnection = new DbConnection("localhost", "blog", "drazen", "Drazen123*");
+    $environment = new Environment();
+
+    $dBConnection = new DbConnection(
+        $environment::SERVERNAME,
+        $environment::DB_NAME,
+        $environment::USERNAME,
+        $environment::PASSWORD
+    );
+
     $connection = $dBConnection->connect();
     $postRepository = new PostRepository($connection);
     $posts = $postRepository->getPosts();
 ?>
 
-<div class="blog-post">
-    <nav class="blog-pagination">
-        <a class="btn btn-outline-primary" href="#">Older</a>
-        <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
-    </nav>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<link href="styles/blog.css" rel="stylesheet">
+<link rel="stylesheet" href="styles/styles.css">
 
-    <div>
-        <?php foreach($posts as $post): ?>
-        <h3>
-           <a href="/single-post.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a> 
-        </h3>
-        <?php endforeach; ?>
+<?php include('header.php') ?>
+
+<main role="main" class="container">
+
+    <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+            <div>
+                <?php foreach($posts as $post): ?>
+                <h1>
+                <a href="/single-post.php?id=<?php echo $post['id']; ?>" class = "single-posts-titles"><?php echo $post['title']; ?></a> 
+                </h1>
+                <p><?php echo $post['body']; ?></p>
+            <?php endforeach; ?>
     </div>
 
 </div>
+<?php include('sidebar.php') ?>
+</main>
+<?php include('footer.php') ?>
+
