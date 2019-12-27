26328a2ca10a57ffdff534adcf376593ade092b9
Drazen Baus <drazen.baus@yahoo.com>
2019-12-27 15:47:02 +0100

    version 2.0

--- comments.php
+++ comments.php
@@ -1,16 +1,29 @@
 <?php
-    
-    $dBConnection = new DbConnection("localhost", "blog", "drazen", "Drazen123*");
+    $dBConnection = new DbConnection(
+        $environment::SERVERNAME,
+        $environment::DB_NAME,
+        $environment::USERNAME,
+        $environment::PASSWORD
+    );
+
     $connection = $dBConnection->connect();
     $postRepository = new PostRepository($connection);
     $comments = $postRepository->getPostComments($_GET["id"]);
    
 ?>
 
-<div>
+<div id = "myDIV">
         <?php foreach($comments as $comment): ?>
-        <p>
-           <?php echo $comment['text']; ?>
-        </p>
+    <ul class = 'single-post-comment-section' >
+    <li class = 'single-post-comments' ><?php echo $comment['text']; ?></li>
+    <li>Commented at <?php echo $post['created_at']?> by <?php echo $comment['author'];?> </li>
+    </ul>
+    <form action="single-post.php?id=<?php echo $post['id']?>&delComment=<?php echo $comment['id']?>" method = 'POST'>    
+        <button type="submit" class="btn btn-danger">Delete comment</button>
+    </form>
+    
         <?php endforeach; ?>
-    </div>
+</div>
+
+
+<script type="text/javascript" src="scripts.js"></script>
